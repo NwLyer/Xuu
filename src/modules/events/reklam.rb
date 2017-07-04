af4950b853b22ac50352c9205cfde97bd9273512
Unknown <osmankaya8@hotmail.com>
2017-07-05 02:57:49 +0300

    reklam fix

--- src/modules/events/reklam.rb
+++ src/modules/events/reklam.rb
@@ -3,13 +3,13 @@ module Bot
     # This event is processed each time the bot succesfully connects to discord.
     module Reklam
       extend Discordrb::EventContainer
-      message(from: "KodBilenAdam,NwLyer" ,in:"#bottest", content:"reklam") do |event|
+      message(from: "KodBilenAdam,NwLyer" , content:"reklam") do |event|
         channel = event.user.voice_channel
         server = event.user.server.id
         BOT.voice_connect(channel)
         sleep(0.5)
         event.voice.play_file('data/reklam.mp3')
-        BOT.voice_destroy(channel)
+        BOT.voice_destroy(channel.server.id)
     end
     end
   end
