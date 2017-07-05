545335295bee3992ea268c6d72ea09138fe7dfb9
Unknown <osmankaya8@hotmail.com>
2017-07-05 14:52:57 +0300

    gizlireklam, bjack bug fixes.

--- src/modules/events/gizlireklam.rb
+++ src/modules/events/gizlireklam.rb
@@ -9,14 +9,9 @@ end
 module Reklam2
   extend Discordrb::EventContainer
   presence(from:'KodBilenAdam' , status: :online) do |event|
+
     BOT.send_message(331104630283042817,'Patron geldi reklam meklam yok.')
 end
 end
-module Reklam3
-  extend Discordrb::EventContainer
-  presence(from:'NwLyer' , status: :online) do |event|
-    BOT.send_message(331139916379586561,'NwLyer is online.')
-end
-      end
   end
 end
