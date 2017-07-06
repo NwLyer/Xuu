72a2b4b0ac001da441f078f69b1f944104e535fb
Unknown <osmankaya8@hotmail.com>
2017-07-06 04:19:21 +0300

    rulet v1

--- src/modules/commands/rulet.rb
+++ src/modules/commands/rulet.rb
@@ -2,18 +2,56 @@ module Bot
   module DiscordCommands
     module Rand
       extend Discordrb::Commands::CommandContainer
-      command(:rand, help_available: false) do |event|
-        break unless event.user.id == CONFIG.owner
-    r = event.send "Kartlar karıştırılıyor. Lütfen bekleyiniz."
-    r.edit ":diamonds: :hearts: :spades: :clubs:"
+      command(:rand, help_available: false) do |event, args|
+        array = [":black_circle",":red_circle"]
+        a1 = array.sample
+        a2 = array.sample
+        a3 = array.sample
+        a4 = array.sample
+        a5 = array.sample
+        a6 = array.sample
+        a7 = array.sample
+        a8 = array.sample
+        a9 = array.sample
+        a10 = array.sample
+        a11 = array.sample
+        a12 = array.sample
+        a13 = array.sample
+        a14 = array.sample
+        a15 = array.sample
+        a16 = array.sample
+        a17 = array.sample
+        win = array.sample
+        a0 = array.sample
+
+    r = event.send "```#{a1}#{a2}#{a3}#{a4}#{a5}I#{a6}I#{a7}#{a8}#{a9}#{a10}#{a0}```"
     sleep(0.1)
-    r.edit ":hearts: :spades: :clubs: :diamonds:"
-    sleep(0.2)
-    r.edit ":spades: :clubs: :diamonds: :hearts:"
-    sleep(0.2)
-    r.edit ":clubs: :diamonds: :hearts: :spades:"
-    sleep(0.2)
-    r.edit ":diamonds: :hearts: :spades: :clubs:"
+            r.edit "```#{a2}#{a3}#{a4}#{a5}#{a6}I#{a7}I#{a8}#{a9}#{a10}#{a0}#{a11}```"
+    sleep(0.1)
+            r.edit "```#{a3}#{a4}#{a5}#{a6}#{a7}I#{a8}I#{a9}#{a10}#{a0}#{a11}#{a12}```"
+    sleep(0.1)
+            r.edit "```#{a4}#{a5}#{a6}#{a7}#{a8}I#{a9}I#{a10}#{a0}#{a11}#{a12}#{win}```"
+    sleep(0.1)
+            r.edit "```#{a5}#{a6}#{a7}#{a8}#{a9}I#{a10}I#{a0}#{a11}#{a12}#{win}#{a13}```"
+    sleep(0.1)
+            r.edit "```#{a6}#{a7}#{a8}#{a9}#{a10}I#{a0}I#{a11}#{a12}#{win}#{a13}#{a14}```"
+    sleep(0.1)
+            r.edit "```#{a7}#{a8}#{a9}#{a10}#{a0}I#{a11}I#{a12}#{win}#{a13}#{a14}#{a15}```"
+    sleep(0.1)
+            r.edit "```#{a8}#{a9}#{a10}#{a0}#{a11}I#{a12}I#{win}#{a13}#{a14}#{a15}#{a16}```"
+    sleep(0.1)
+            r.edit "```#{a9}#{a10}#{a0}#{a11}#{a12}I#{win}I#{a13}#{a14}#{a15}#{a16}#{a17}```"
+
+if win == ":black_circle"
+  wrenk = black
+elsif win == "red_circle"
+  wrenk = red
+end
+            if args == wrenk
+              event.send "Kazandınız."
+            else
+              event.send "Kaybettiniz."
+            end
       end
     end
   end
